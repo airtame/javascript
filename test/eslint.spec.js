2de39480f013090d38a838da9042939c23fa17c8
Marcos Mellado <marcos@mellados.com>
2017-09-11 12:01:40 +0200

    Adding prettier to the config

--- test/eslint.spec.js
+++ test/eslint.spec.js
@@ -1,16 +1,21 @@
 import chai, { expect } from 'chai';
-import {CLIEngine} from 'eslint';
+import { CLIEngine } from 'eslint';
 
 describe('eslint', () => {
-
   it('should validate the .eslintrc configuration file', () => {
-
     const cli = new CLIEngine({
       useEslintrc: false,
-      configFile: './eslintrc.json'
-    })
+      configFile: './eslintrc.json',
+    });
 
-    expect(cli.executeOnText('// this is a comment\n').errorCount).to.equal(0);
+    const report = cli.executeOnText('var x =  0;');
+    const report2 = cli.executeOnText('// this is a coment\n');
+    const report3 = cli.executeOnText('const x =  0;\n');
+    const report4 = cli.executeOnText('let x = 1\n');
+
+    expect(report.results[0].errorCount).to.equal(3);
+    expect(report2.results[0].errorCount).to.equal(0);
+    expect(report3.results[0].errorCount).to.equal(2);
+    expect(report4.results[0].errorCount).to.equal(3);
   });
-
 });
